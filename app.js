4781347ab94ee4db338bec415247a52a5b62cbd9
Shubham Meshram <meshram.shubham27@gmail.com>
2023-07-07 23:15:57 +0530

    added eventlistener

---
+++ app.js
@@ -0,0 +1,6 @@
+const keyName = document.getElementById('key_name');
+const keyCode = document.getElementById('key_code');
+
+document.addEventListener('keypress', (event) => {
+    console.log(event);
+});
