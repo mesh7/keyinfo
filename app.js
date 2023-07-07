83a8a531b93aa2c945fbeba06b5ba5d23ad055cf
Shubham Meshram <meshram.shubham27@gmail.com>
2023-07-08 03:06:06 +0530

    added styling for card and logic to display key info

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-const keyName = document.getElementById('key_name');
-const keyCode = document.getElementById('key_code');
+const keyboardKey = document.getElementById('keyboard_key');
+const keyboardCode = document.getElementById('keyboard_code');
+const keyboardLocation = document.getElementById('keyboard_location');
+const keyWhich = document.getElementById('keyboard_which');
 
 document.addEventListener('keypress', (event) => {
-    console.log(event);
+    keyboardKey.innerHTML = event.key;
+    keyboardCode.innerHTML = event.code;
+    keyboardLocation.innerHTML = event.location;
+    keyWhich.innerHTML = event.which;
 });
