ba0f84f3ba1489b5d4fccc76a6673d7760ceabb0
Shubham Meshram <meshram.shubham27@gmail.com>
2023-07-15 22:23:43 +0530

    removed the depricated keypress

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const keyboardCode = document.getElementById('keyboard_code');
 const keyboardLocation = document.getElementById('keyboard_location');
 const keyWhich = document.getElementById('keyboard_which');
 
-document.addEventListener('keypress', (event) => {
+document.addEventListener('keydown', (event) => {
     keyboardKey.innerHTML = event.key;
     keyboardCode.innerHTML = event.code;
     keyboardLocation.innerHTML = event.location;
